05e7211c9c149af713cbf6854faf329569cb88b4
Ji Seungmin <cswcsm02@gmail.com>
2022-12-20 18:41:58 +0900

    의존성 주입 Module 생성

--- app/src/main/java/com/example/booksearchapp/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/booksearchapp/data/api/RetrofitInstance.kt
@@ -1,40 +1,34 @@
 package com.example.booksearchapp.data.api
 
-import com.example.booksearchapp.util.Constants.BASE_URL
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-
 /**
  * Api 객체를 만들고 요청을 보내주는 인스턴스
  * object와 lazyz키워드로 구현함으로 실제 사용되는 순간 단 하나의 Retrofit 인스턴스가 만들어지도록 싱글톤으로 구현
  * */
-object RetrofitInstance {
-
-    //LAZY를 사용하는 이유: 여러개의 Retrofit 객체가 만들어지면 자원도 낭비되고 통신에 혼선이 발생하므로
-    //스프링의 LazyLoading과 비슷
-    private val okHttpClient: OkHttpClient by lazy {
-        val httpLoggingInterceptor = HttpLoggingInterceptor()
-            .setLevel(HttpLoggingInterceptor.Level.BODY)
-        OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
-    }
-
-    //Retrofit 객체 생성
-    private val retrofit: Retrofit by lazy {
-        //DTO 변환에 사용할 MoshiConverterFactory를 Json Converter로 지정
-        //OkHttp Core는 서버와 애플리케이션 사이에서 데이터를 인터셉터하는 기능이 있어 okHttpClient 인터셉터를 넘겨주어 로그 캣에서 패킷 내용 모니터링
-        Retrofit.Builder()
-            .addConverterFactory(MoshiConverterFactory.create())
-            .client(okHttpClient).baseUrl(BASE_URL).build() //build로 객체 생성
-    }
-
-    val api: BookSearchApi by lazy {
-        //BookSearchApi 객체 생성
-        retrofit.create(BookSearchApi::class.java)
-    }
-
-    fun getInstance() : Retrofit {
-        return retrofit
-    }
-}
+//object RetrofitInstance {
+//
+//    //LAZY를 사용하는 이유: 여러개의 Retrofit 객체가 만들어지면 자원도 낭비되고 통신에 혼선이 발생하므로
+//    //스프링의 LazyLoading과 비슷
+//    private val okHttpClient: OkHttpClient by lazy {
+//        val httpLoggingInterceptor = HttpLoggingInterceptor()
+//            .setLevel(HttpLoggingInterceptor.Level.BODY)
+//        OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
+//    }
+//
+//    //Retrofit 객체 생성
+//    private val retrofit: Retrofit by lazy {
+//        //DTO 변환에 사용할 MoshiConverterFactory를 Json Converter로 지정
+//        //OkHttp Core는 서버와 애플리케이션 사이에서 데이터를 인터셉터하는 기능이 있어 okHttpClient 인터셉터를 넘겨주어 로그 캣에서 패킷 내용 모니터링
+//        Retrofit.Builder()
+//            .addConverterFactory(MoshiConverterFactory.create())
+//            .client(okHttpClient).baseUrl(BASE_URL).build() //build로 객체 생성
+//    }
+//
+//    val api: BookSearchApi by lazy {
+//        //BookSearchApi 객체 생성
+//        retrofit.create(BookSearchApi::class.java)
+//    }
+//
+//    fun getInstance() : Retrofit {
+//        return retrofit
+//    }
+//}
--- app/src/main/java/com/example/booksearchapp/data/db/BookSearchDatabase.kt
+++ app/src/main/java/com/example/booksearchapp/data/db/BookSearchDatabase.kt
@@ -1,8 +1,6 @@
 package com.example.booksearchapp.data.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.example.booksearchapp.data.model.Book
@@ -24,26 +22,26 @@ abstract class BookSearchDatabase : RoomDatabase() {
     //Room에서 사용할 dao 지정
     abstract fun bookSearchDao(): BookSearchDao
 
-    //DB 객체도 생성하는데 비용이 많이 들기 때문에 중복으로 생성하지 않도록 싱글톤 설정
-    companion object {
-
-        //Java변수를 Main Memory에 저장하겠다는 것을 명시하는 것
-        //Multi-Thread 환경을 대비하기 위해 사용
-        @Volatile
-        private var INSTANCE: BookSearchDatabase? = null
-
-        private fun buildDatabase(context: Context): BookSearchDatabase =
-            Room.databaseBuilder(
-                context.applicationContext,
-                BookSearchDatabase::class.java,
-                "favorite-books"
-            ).build()
-
-        fun getInstance(context: Context): BookSearchDatabase =
-            INSTANCE ?: synchronized(this) {
-                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
-            }
-    }
+//    //DB 객체도 생성하는데 비용이 많이 들기 때문에 중복으로 생성하지 않도록 싱글톤 설정
+//    companion object {
+//
+//        //Java변수를 Main Memory에 저장하겠다는 것을 명시하는 것
+//        //Multi-Thread 환경을 대비하기 위해 사용
+//        @Volatile
+//        private var INSTANCE: BookSearchDatabase? = null
+//
+//        private fun buildDatabase(context: Context): BookSearchDatabase =
+//            Room.databaseBuilder(
+//                context.applicationContext,
+//                BookSearchDatabase::class.java,
+//                "favorite-books"
+//            ).build()
+//
+//        fun getInstance(context: Context): BookSearchDatabase =
+//            INSTANCE ?: synchronized(this) {
+//                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+//            }
+//    }
 
     //ROOM은 orm 수행시 Primitive 타입과 Boxed 타입만 사용하도록 제한
     //Room에서 일반 객체로 orm을 수행하면 ui 쓰레드에서 lazy로딩을 해야하는데 그러면 처리 속도가 느려지고
