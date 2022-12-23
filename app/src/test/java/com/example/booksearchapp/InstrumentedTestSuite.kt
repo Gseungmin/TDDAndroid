0c566c4c2b1ec56cfdc1a8ca7ec024297b01550e
Ji Seungmin <cswcsm02@gmail.com>
2022-12-23 21:26:49 +0900

    JUNIT 테스트 구현 완료

---
+++ app/src/test/java/com/example/booksearchapp/InstrumentedTestSuite.kt
@@ -0,0 +1,16 @@
+package com.example.booksearchapp
+
+import com.example.booksearchapp.ui.view.MainActivityTest
+import com.example.booksearchapp.util.CalculatorTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@ExperimentalCoroutinesApi
+@Suite.SuiteClasses(
+    MainActivityTest::class,
+    CalculatorTest::class,
+)
+class InstrumentedTestSuite {
+}
