b1fae77e926d769ac4947a13acf573247699f735
Ji Seungmin <cswcsm02@gmail.com>
2022-12-24 16:23:32 +0900

    test

--- app/src/test/java/com/example/booksearchapp/util/ConstantsTest.kt
+++ app/src/test/java/com/example/booksearchapp/util/ConstantsTest.kt
@@ -1,8 +1,10 @@
 package com.example.booksearchapp.util
 
 import androidx.test.filters.SmallTest
+import com.example.booksearchapp.BuildConfig
 import com.example.booksearchapp.util.Constants.API_KEY
 import com.google.common.truth.Truth
+import org.junit.Assert.*
 import org.junit.Test
 
 @SmallTest
@@ -12,7 +14,7 @@ class ConstantsTest {
     @Test
     fun `key test`() {
         // Given
-        val key = API_KEY
+        val key = BuildConfig.API_KEY
         val value = "937eecb78ab4f0780521a9f149517547"
 
         Truth.assertThat(key).isEqualTo(value)
